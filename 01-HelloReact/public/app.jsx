0f58c907cf1a2ea1dce90f40e66b828595f38c4d
leopoldkwok <leopoldkwok@gmail.com>
2018-02-28 08:39:44 +0000

    User Events  Callbacks

--- 01-HelloReact/public/app.jsx
+++ 01-HelloReact/public/app.jsx
@@ -6,6 +6,13 @@ var Greeter = React.createClass({
 
     };
   },
+  onButtonClick: function(e) {
+    e.preventDefault();
+
+    var name = this.refs.name.value;
+
+    alert(name);
+  },
   render: function(){
     var name = this.props.name;
     var message = this.props.message;
@@ -14,6 +21,11 @@ var Greeter = React.createClass({
       <div>
         <h1>Hello {name}!</h1>
         <p>{message + '!!'}</p>
+
+        <form onSubmit={this.onButtonClick}>
+          <input type="text" ref="name"/>
+          <button>Set Name</button>
+        </form>
       </div>
     );
   }
